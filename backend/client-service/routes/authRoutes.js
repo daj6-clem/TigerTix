f8f7eab950b4029bd39b6b4141522c36410211d0
daj6-clem <danielajonescpsc@gmail.com>
2025-11-19 17:46:22 -0500

    double checking route lock in event of bad token

--- backend/client-service/routes/authRoutes.js
+++ backend/client-service/routes/authRoutes.js
@@ -2,6 +2,7 @@
 
 import express from "express";
 import {register, login, logout, getCurrentUser} from "../controllers/authController.js";
+import {verifyToken} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
